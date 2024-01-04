0cb514f7b759af885335f008db0c25b981929131
0xf00 <fulvio.meden@prima.it>
2024-01-04 16:30:57 +0000

    enh: preparing new release version

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule CloudflareAccessEx.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/primait/cloudflare_access_ex"
-  @version "0.1.2"
+  @version "0.1.3"
 
   def project do
     [
